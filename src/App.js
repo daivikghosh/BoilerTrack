0119c96b29ccd8f53f7bc57b3a70cac9787cbfe6
aniwag2 <aniwaghray@gmail.com>
2024-09-28 17:29:42 -0400

    Image Upload page is created, needs styling and connection to database

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginSignupPage from "./pages/LoginSignupPage";
+import ImageUpload from './pages/ImageUpload';
 import Layout from "./components/Layout/Layout.js";
 
 const App = () => {
@@ -8,7 +9,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<LoginSignupPage />} />{" "}
-        {/* Root URL shows the login/signup page */}
+        <Route path="/UploadImage" element={<ImageUpload />} />  {/* Add this line */}
       </Routes>
     </Router>
   );
--- src/pages/ImageUpload.js
+++ src/pages/ImageUpload.js
@@ -1,6 +1,14 @@
-import React, { useState } from "react";
+import React from 'react';
+import ImageUploadForm from '../components/ImageUploadForm/ImageUploadForm';
 
-const ImageUpload = () => {
+const ImageUploadPage = () => {
+  return (
+    <div>
+      <h1>Upload an Image</h1>
+      <ImageUploadForm />
+      {/* add more as needed */}
+    </div>
+  );
 };
 
-export default ImageUpload;
+export default ImageUploadPage;
