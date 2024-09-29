24d2a3ecaac756f64982f7bb4ab67bbe76dcce76
Daivik Ghosh <112281943+daivikghosh@users.noreply.github.com>
2024-09-29 18:38:23 -0400

    Fixed Errors with CreateUserDB.py
    Fixing Errors

--- UserData/CreateUserDB.py
+++ UserData/CreateUserDB.py
@@ -6,12 +6,15 @@ def create_user_database():
 
     #TODO: Add Pre-Registered Items Field
 
+    cursor.execute('DROP TABLE IF EXISTS UserListing') #Delete the Table if it already exists to re create it
+
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS UserListing 
         (
             UserID      INTEGER PRIMARY KEY AUTOINCREMENT,
             Email       TEXT    NOT NULL    UNIQUE,
             Password    TEXT    NOT NULL,
+            Name        TEXT    NOT NULL,
             isStudent   INTEGER,
             isStaff     INTEGER
         )''')
@@ -20,6 +23,3 @@ def create_user_database():
     
     conn.commit()
     conn.close()
-
-if __name__ == "__main__":
-    create_user_database()
