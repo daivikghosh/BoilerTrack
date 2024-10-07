1c90b18ffc23a4588a8ced22da0d7efeabe70d78
WhiffyMuffinz <88297992+WhiffyMuffinz@users.noreply.github.com>
2024-10-07 15:17:42 -0400

    removed frontend and backend folders

--- .devcontainer/on-create-command.sh
+++ .devcontainer/on-create-command.sh
@@ -2,6 +2,5 @@
 set -e
 python3 -m venv --upgrade-deps backend/.venv --prompt BoilerTrack
 . ./backend/.venv/bin/activate
-pip install -r ./backend/requirements.txt
-cd frontend
+pip install -r ./flask-server/requirements.txt
 npm i
