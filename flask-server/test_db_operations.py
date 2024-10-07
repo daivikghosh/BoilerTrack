d6c98f9110cb947de0c57eb7a6a7e33bdcf33ca1
Shlok Bairagi <110861439+WSDx07@users.noreply.github.com>
2024-10-06 21:00:45 -0400

    Update test_db_operations.py
    Fixing issues with unit test

--- flask-server/test_db_operations.py
+++ flask-server/test_db_operations.py
@@ -1,14 +1,14 @@
 import sqlite3
 from AddFoundItemPic import insertItem
 
-def get_item_by_id(ItemID):
+def get_item_by_name(ItemName):
     """
     Helper function to retrieve an item from the database by ItemID.
     """
     conn = sqlite3.connect('databases/ItemListings.db')
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM FOUNDITEMS WHERE ItemID = ?", (ItemID,))
+    cursor.execute("SELECT * FROM FOUNDITEMS WHERE ItemName = ?", (ItemName,))
     item = cursor.fetchone()
 
     conn.close()
@@ -16,14 +16,15 @@ def get_item_by_id(ItemID):
 
 def main():
     # Create an item in the database
-    insertItem(1, "Red wallet, leather", "Library", "Lost and Found")
+    #insertItem(1, "Red wallet, leather", "Library", "Lost and Found")
+    insertItem("UnitTest", "TESTColor", "TESTBrand", "TESTFound", "TESTTurned", "TESTDesc", "uploads/TestImage.png")
     print("\nItem added to the database.")
 
     # Test Added Item
-    retrieved_item = get_item_by_id(1)
+    retrieved_item = get_item_by_name("UnitTest")
     if retrieved_item:
         print("\nItem retrieved from the database:")
-        print(f"ItemID: {retrieved_item[0]}, Keywords: {retrieved_item[1]}, LocationFound: {retrieved_item[2]}, LocationTurnedIn: {retrieved_item[3]}")
+        print(f"ItemID: {retrieved_item[0]}, Keywords: {retrieved_item[1]+","+retrieved_item[2]+","+retrieved_item[3]}, LocationFound: {retrieved_item[4]}, LocationTurnedIn: {retrieved_item[5]}")
     else:
         print("\nError: Item not found in the database.")
 
