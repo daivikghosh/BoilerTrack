da12a875fe925039348a6fa5006b8ea89f1c1277
WhiffyMuffinz <88297992+WhiffyMuffinz@users.noreply.github.com>
2024-10-07 22:58:00 -0400

    update on-create-command

--- .devcontainer/on-create-command.sh
+++ .devcontainer/on-create-command.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-python3 -m venv --upgrade-deps backend/.venv --prompt BoilerTrack
-. ./backend/.venv/bin/activate
+python3 -m venv --upgrade-deps flask-server/.venv --prompt BoilerTrack
+. ./flask-server/.venv/bin/activate
 pip install -r ./flask-server/requirements.txt
 npm i
