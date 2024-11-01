4ec00f6a705a79b746d1f32b7d3ea0db8dba351d
WhiffyMuffinz <88297992+WhiffyMuffinz@users.noreply.github.com>
2024-11-01 01:01:54 -0400

    did the thing minus a few sorting buttons

--- .devcontainer/on-create-command.sh
+++ .devcontainer/on-create-command.sh
@@ -4,4 +4,3 @@ python3 -m venv --upgrade-deps flask-server/.venv --prompt BoilerTrack
 . ./flask-server/.venv/bin/activate
 pip install -r ./flask-server/requirements.txt
 npm i
-export BROWSER=none
