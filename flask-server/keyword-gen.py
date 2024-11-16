fb25ac238fa8fbbd1318761a2b70f70245da66c1
WhiffyMuffinz <88297992+WhiffyMuffinz@users.noreply.github.com>
2024-11-16 23:35:12 +0000

    file with functions straight from the docs

---
+++ flask-server/keyword-gen.py
@@ -0,0 +1,57 @@
+from google.cloud import vision
+
+
+def detect_logos(path):
+    """Detects logos in the file."""
+
+    client = vision.ImageAnnotatorClient()
+
+    with open(path, "rb") as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.logo_detection(image=image)
+    logos = response.logo_annotations
+    print("Logos:")
+
+    # for logo in logos:
+    #     print(logo.description)
+
+    if response.error.message:
+        raise Exception(
+            "{}\nFor more info on error messages, check: "
+            "https://cloud.google.com/apis/design/errors".format(
+                response.error.message)
+        )
+    return logos
+
+
+def detect_labels(path):
+    """Detects labels in the file."""
+
+    client = vision.ImageAnnotatorClient()
+
+    with open(path, "rb") as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    print("Labels:")
+
+    # for label in labels:
+    # print(label.description)
+
+    if response.error.message:
+        raise Exception(
+            "{}\nFor more info on error messages, check: "
+            "https://cloud.google.com/apis/design/errors".format(
+                response.error.message)
+        )
+    return labels
+
+
+if __name__ == '__main__':
+    print(detect_logos('uploads/HyFbottle.jpg'))
